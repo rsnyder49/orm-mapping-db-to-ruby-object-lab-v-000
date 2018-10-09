c9c73da014c532c95a4391696157d6af035097af
Robert Snyder <r.snyder49@gmail.com>
2018-10-09 18:28:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
   
   def self.first_student_in_grade_10 
     sql = <<-SQL
-      SELECT *
+      SELECT students.grade 
       FROM STUDENTS 
       WHERE grade == 10
       LIMIT 1 

47de80cf7e32359130c62f207f566799a4917090
Robert Snyder <r.snyder49@gmail.com>
2018-10-09 18:30:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,6 +62,17 @@ class Student
       self.new_from_db(row)
     end.first
   end
+  
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade == ? 
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL 

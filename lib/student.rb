8ce20d75cb6c38a5aa25630c449f441689aa4c98
Robert Snyder <r.snyder49@gmail.com>
2018-10-09 18:26:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,7 +46,9 @@ class Student
       WHERE grade == 10
       LIMIT 2
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end 
 
   def self.find_by_name(name)
